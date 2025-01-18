604f4a8b31b338da032403f76eba0fcff9bdbcde
Martin Kulich <kulda.zbraslav@gmail.com>
2025-01-18 17:57:39 +0100

    Zoom out

--- config.py
+++ config.py
@@ -4,4 +4,4 @@ class Config:
   SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL').replace('postgres://', 'postgresql://') if os.environ.get('DATABASE_URL') else 'sqlite:///:memory:'
   SQLALCHEMY_TRACK_MODIFICATIONS = False
   MAP_CENTER = [50, 15]
-  MAP_ZOOM = 7
+  MAP_ZOOM = 6
